5674534956866207d2e3b852411198663ab68a64
YazanAwawda <yazanmah88@gmail.com>
2022-12-22 15:14:48 +0200

    Add existing project files to Git

---
+++ PracticesPart1/Program.cs
@@ -0,0 +1,27 @@
+﻿class person { 
+    public string ? FirstName { get; private set; }
+    public string? LastName { get; private set; }
+
+    public person(String firstName, String lastName) { 
+    this.FirstName = firstName;
+    this.LastName = lastName;
+    }
+    public virtual String GetFullName() {
+        return String.Format("{0} {1}" , FirstName , LastName);
+    }
+}
+// Child class fields should not hide parent class fields
+class employee : person { 
+
+    public String Role { get; private set; }
+
+    public employee(String firstName, String lastName, String role) : base(firstName, firstName)
+    {
+        Role = role;
+    }
+
+    public override string GetFullName()
+    {
+        return base.GetFullName();
+    }
+}
